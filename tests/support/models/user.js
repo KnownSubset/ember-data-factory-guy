54c4e0b9a88f06795c929d3c235e1b56439fe775
Nathan Dauber <nathan@radialogica.com>
2014-09-05 10:03:31 -0500

    Tests fixes from updating Ember-Data
    The latest ember-data beta.9 was released with an issue where non-async
    relationships were being resolved as promises.  This caused issues with
    the tests.  Upgrading to the canary resolves the test failures.

--- tests/support/models/user.js
+++ tests/support/models/user.js
@@ -2,7 +2,7 @@ User = DS.Model.extend({
   name:       DS.attr('string'),
   company:    DS.belongsTo('company', {async: true, inverse: 'users'}),
   properties: DS.hasMany('property', {async: true, inverse: 'owners'}),
-  projects:   DS.hasMany('project', {async: true}),
+  projects:   DS.hasMany('project'),
   hats:       DS.hasMany('hat', {polymorphic: true})
 });
 
